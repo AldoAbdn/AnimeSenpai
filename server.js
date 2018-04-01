41fc4eccc2f826139b0a228e078ec4f450383d12
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-01 13:42:35 +0100

    check

--- server.js
+++ server.js
@@ -93,7 +93,7 @@ app.get("/popup/anime/reviews", function(req,res){
 });
 app.get("/popup/anime/streaming", function(req,res){
     console.log(streamingSiteData);
-    let sites = streamingSiteData.filter(function(anime){return anime.name.toLowerCase().includes(req.query.anime.toLowerCase())});
+    let sites = streamingSiteData.filter(function(item){return item.name.includes(req.query.anime.toLowerCase())});
     console.log(req.query.anime);
     res.send(JSON.stringify(sites));
 });
