ed120246da72bdb059a58d479dfa827966e37e05
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-08 12:29:27 +0100

    spelling error

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.get("/home/bestindie", function(req,res){
 });
 app.get("/home/search", function(req,res){
     let search = req.query.search.toLowerCase();
-    https.get(animeNewsNetworkApi.animeNewsNetworkReportUrl + "&search=" + search, responce => {
+    https.get(animeNewsNetworkApi.animeNewsNetworkReportUrl + "&search=" + search, response => {
         let result = "";
         response.on("data", data => {
             result += data;
