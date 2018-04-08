6b793e1495e74597bdc5cd9b199ee5adcd0e6412
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-08 19:56:09 +0100

    Fix for now will revise later

--- public/js/animeSenpai.js
+++ public/js/animeSenpai.js
@@ -85,12 +85,12 @@ animeSenpai.controller("homeController", function($scope,$http){
   $('#brand').css('visibility','hidden');
   //Example of what might be returned from server
   $scope.home = {
-    anime: {specialBlend:[{id:1,summary:"summary Goes here",size:"anime-5",img:"/images/about_img.jpg)"},
-                          {id:1,summary:"summary Goes here",size:"anime-1",img:{"background-img":"url(/imgs/about_img.jpg)"}},
-                          {id:1,summary:"summary Goes here",size:"anime-3",img:{"background-img":"url(/imgs/about_img.jpg)"}},
-                          {id:1,summary:"summary Goes here",size:"anime-2",img:{"background-img":"url(/imgs/about_img.jpg)"}},
-                          {id:1,summary:"summary Goes here",size:"anime-4",img:{"background-img":"url(/imgs/about_img.jpg)"}},
-                          {id:1,summary:"summary Goes here",size:"anime-5",img:{"background-img":"url(/imgs/about_img.jpg)"}}],
+    anime: {specialBlend:[{id:1,summary:"summary Goes here",size:"anime-5",img:"/images/about_img.jpg"},
+                          {id:1,summary:"summary Goes here",size:"anime-1",img:"/images/about_img.jpg"},
+                          {id:1,summary:"summary Goes here",size:"anime-3",img:"/images/about_img.jpg"},
+                          {id:1,summary:"summary Goes here",size:"anime-2",img:"/images/about_img.jpg"},
+                          {id:1,summary:"summary Goes here",size:"anime-4",img:"/images/about_img.jpg"},
+                          {id:1,summary:"summary Goes here",size:"anime-5",img:"/images/about_img.jpg"}],
             searchResults: []
           },
     search:""
