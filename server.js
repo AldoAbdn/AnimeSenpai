c9b681ca501eea2b985cf206860e163849348e77
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-08 12:50:35 +0100

    testing

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const animeNewsNetworkApi = {
             res.on("end", () => {
                 xmlParser.parseString(result, (err,result)=>{
                     if (err) throw err;
-                    callback(result.ann.anime[0]);
+                    callback(result.anime[0]);
                 })
             });
         });
