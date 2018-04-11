806c7537f0f9a738d9835d45904d567d6057e069
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-11 12:56:06 +0100

    Fix

--- server.js
+++ server.js
@@ -168,7 +168,7 @@ app.get("/comments", async function(req,res){
     //Returns comments related to a parent by id
     //Need to write a recursive function that returns an array of comments that is appended to replies 
     let commentTest = await getComments(req.body.id);
-    console.log(commenTest);
+    console.log(commentTest);
     let comments = [{comment:"I AM A COMMENT",author:"Aldo",date:Date(),replies:[{comment:"I AM A COMMENT",author:"Aldo",date:Date(),replies:[]}]}];
     res.send(JSON.stringify(comments));
 });
