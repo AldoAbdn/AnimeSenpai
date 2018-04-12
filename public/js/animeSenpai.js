2c84c436b1f7f4f1163f217d5a5a727bd9defa49
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-12 09:28:15 +0100

    trying to fix

--- public/js/animeSenpai.js
+++ public/js/animeSenpai.js
@@ -340,7 +340,8 @@ animeSenpai.directive("reply", function($compile){
     template:"",
     link: function (scope, element, attrs){
       if(reply.length > 0){
-        element.append("<div comments='reply'></div>")
+        element.append("<div comments='reply'></div>");
+        $compile(element.conteante())(scope);
       }
     }
   }
