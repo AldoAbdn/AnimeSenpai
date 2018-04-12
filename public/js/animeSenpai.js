d52b29f362f91e7a924e2e764a1b31bf0b9f49b8
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-12 09:03:04 +0100

    test

--- public/js/animeSenpai.js
+++ public/js/animeSenpai.js
@@ -130,8 +130,7 @@ animeSenpai.controller("mainController", function($scope,$location,$timeout,$htt
     $http.get("/comments",{params:{id:$scope.clickedItem.id}})
     .then(function(response){
       //let commentsHtml = createCommentHtml(response.data);
-      let test = [{comment:"Base Comment",replies:[{comment:"Reply 1",replies:[]},
-                                                   {comment:"Reply2",replies:[]}]
+      let test = [{comment:"Base Comment",replies:[]
                   }];
       let commentsHtml = createCommentHtml(test);
       //callback(response.data,commentsHtml);
