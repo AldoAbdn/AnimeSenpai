a43e6e277d8f2c81756e8cc398a08c66536115e8
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-13 22:12:35 +0100

    Adding dropdown navigation

--- public/js/animeSenpai.js
+++ public/js/animeSenpai.js
@@ -279,6 +279,7 @@ animeSenpai.controller("loginDropdown", function($scope, $http){
     $http.post("/login",{params:{email:$scope.email,password:$scope.password}})
     .then(function success(response){
       $scope.setProfile(response);
+      $scope.setDropdown("dropdown/profile.html");
      }, function failure(response){
       $scope.warningMessage = "Incorrect login details";
     });
@@ -295,6 +296,7 @@ animeSenpai.controller("signUpDropdown", function($scope){
     $http.post("/signup",{params:{email:$scope.email,password:$scope.password}})
     .then(function success(response){
       $scope.setProfile(response);
+      $scope.setDropdown("dropdown/profile.html");
      }, function failure(response){
       $scope.warningMessage = "Incorrect login details";
     });
