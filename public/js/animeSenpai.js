9c99b8113d0b55e7cd45fb8d0159eeadb4e8327f
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-14 09:30:36 +0100

    home

--- public/js/animeSenpai.js
+++ public/js/animeSenpai.js
@@ -106,14 +106,14 @@ animeSenpai.controller("homeController", function($scope,$http){
   $http.get("/home/get")
   .then(function(response){
     $scope.home = response.data;
-    console.log(response.data);
     //for testing
-    $scope.home.specialBlend = [{id:1,summary:"summary Goes here",size:"anime-5",img:"/images/about_img.jpg"},
+    $scope.home.anime.specialBlend = [{id:1,summary:"summary Goes here",size:"anime-5",img:"/images/about_img.jpg"},
     {id:1,summary:"summary Goes here",size:"anime-1",img:"/images/about_img.jpg"},
     {id:1,summary:"summary Goes here",size:"anime-3",img:"/images/about_img.jpg"},
     {id:1,summary:"summary Goes here",size:"anime-2",img:"/images/about_img.jpg"},
     {id:1,summary:"summary Goes here",size:"anime-4",img:"/images/about_img.jpg"},
     {id:1,summary:"summary Goes here",size:"anime-5",img:"/images/about_img.jpg"}];
+    
   });
   //Search Bar Input
   $scope.inputChange = function(){
