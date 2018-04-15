a154afcbeb17fd8695f0557c88f5cf2fb1607375
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-15 10:53:46 +0100

    testing

--- server.js
+++ server.js
@@ -279,11 +279,11 @@ app.get("/popup/anime", async function(req,res){
     let anime = {};
     anime.threads = await db.collection("threads").find({id:req.query.id}).toArray();
     for (let thread in anime.threads){
-        thread.comments = await comments(thread.id);
+        thread.comments = await comments(thread._id);
     }
     anime.reviews = await db.collection("reviews").find({id:req.query.id}).toArray();
     for (let review in anime.reviews){
-        review.comments = await comments(review.id);
+        review.comments = await comments(review._id);
     }
     anime.streaming =  streamingSiteData.filter(function(item){return req.query.title.toLowerCase().indexOf(item.name.toLowerCase()) != -1});
     res.send(JSON.stringify(anime));
