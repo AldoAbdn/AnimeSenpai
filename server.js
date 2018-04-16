17d503b76cc4e7945c0912fa9a27e5ec367934d7
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-16 21:21:45 +0100

    testing

--- server.js
+++ server.js
@@ -159,7 +159,7 @@ app.get("/home/get",async function(req,res){
     }
     animeNewsNetworkApi.getById(ids,anime=>{
         home.anime.specialBlend = anime.filter(anime=>{home.anime.specialBlend.forEach(item=>{if(anime.id==item)return true;});return false;});
-        home.anime.classics = anime.filter(anime=>{home.anime.classics..forEach(item=>{if(anime.id==item)return true;});return false;});
+        home.anime.classics = anime.filter(anime=>{home.anime.classics.forEach(item=>{if(anime.id==item)return true;});return false;});
         home.anime.bestAmerican = anime.filter(anime=>{home.anime.bestAmerican.forEach(item=>{if(anime.id==item)return true;});return false;});
         home.anime.bestIndie = anime.filter(anime=>{home.anime.bestIndie.forEach(item=>{if(anime.id==item)return true;});return false;});
         res.send(JSON.stringify(home));
