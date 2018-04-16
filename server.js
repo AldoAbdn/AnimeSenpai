eb745860bc097a9afd1b79e2dcb3c4f7b0910408
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-16 15:13:16 +0100

    testing

--- server.js
+++ server.js
@@ -197,7 +197,8 @@ app.get("/profileedit/profile",function(req,res){
     res.send(JSON.stringify(profileEdit));
 });
 app.post("/profileedit/profile/edit",async function(req,res){
-    req.session.user = await db.collection("profiles").update({_id:req.session.user._id},{email:req.body.params.profile.email,password:req.body.params.profile.password},{upsert:true})
+    req.session.user = await db.collection("profiles").update({_id:req.session.user._id},{email:req.body.params.profile.email,password:req.body.params.profile.password1},{upsert:true})
+    console.log(req.session.user);
     res.send(200);
 });
 //Thread Edit
