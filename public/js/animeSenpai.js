73a04105573d61d72b65b3c0438c8a0751c815ee
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-17 18:17:57 +0100

    Test

--- public/js/animeSenpai.js
+++ public/js/animeSenpai.js
@@ -403,8 +403,9 @@ animeSenpai.controller("signUpDropdown", function($scope,$http){
     $scope.setDropdown("dropdown/login.html");
   };
   $scope.checkPassword = function(){
-    if ($scope.password == "" || $scope.password2 == ""){$scope.warningMessage=""};
-    if ($scope.password1 != $scope.password2){
+    if ($scope.password == "" || $scope.password2 == ""){
+      $scope.warningMessage="";
+    }else if ($scope.password1 != $scope.password2){
       $scope.warningMessage = "Password Do Not Match";
     }
   }
