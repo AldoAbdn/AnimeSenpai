27f9d07f4441400601aada3313bf83faaf46533e
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-18 10:28:30 +0100

    testing

--- server.js
+++ server.js
@@ -382,7 +382,10 @@ app.post("/login", async function(req,res){
     //req.body.email;req.body.password;
     var email = req.body.params.email;
     var password = req.body.params.password;
+    console.log(email + " " + password);
     let profile = await db.collection("profiles").findOne({email:email,password:password});
+    let profiles = await db.collection("profiles").find().toArray();
+    console.log(profiles);
     if(profile == null){res.sendStatus(401);return;};
     if(profile.password!=undefined && profile.password == password){
       profile.password = null;
