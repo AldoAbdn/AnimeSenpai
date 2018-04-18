1faa7f2cc0458c0aa09ee1712d8c2977682756e8
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-18 20:47:21 +0100

    testing

--- server.js
+++ server.js
@@ -533,7 +533,7 @@ app.get("/popup/anime", async function(req,res){
     //Returns details about an anime from AnimeNetwork api
     //and whatever we have stored
     let anime = req.query.anime;
-    console.log()
+    console.log(anime);
     anime.threads = await db.collection("threads").find({id:anime.id}).toArray();
     for (let thread of anime.threads){
         thread.comments = await getComments(thread._id);
