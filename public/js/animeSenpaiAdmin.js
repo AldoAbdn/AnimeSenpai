fb627e3438799b2eb0f6dde0a94cc257372ab80d
AldoAbdn <alistairaldo@hotmail.co.uk>
2018-04-20 10:26:14 +0100

    fix

--- public/js/animeSenpaiAdmin.js
+++ public/js/animeSenpaiAdmin.js
@@ -207,7 +207,6 @@ animeSenpaiAdmin.controller("listsController", function($scope,$http){
     $http.delete("/admin/lists/delete",{params:{id:anime.id,list:list}})
     .then(function(response){
       $scope.getLists();
-      $scope.anime = null;
     },function(response){
       $scope.navigate("/");
     });
